a3524757585ddcbc696fea972de828d853795adf
Aryles <aryles_77@icloud.com>
2024-04-16 15:58:24 +0200

    ajout du projet

---
+++ assets/js/question.js
@@ -0,0 +1,225 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Comment appelle-t-on la partie en plastique ou métal se trouvant au bout du lacet",
+        answer: "C. Aglet",
+        options:[
+            "A. Onglet",
+            "B. Singlet",
+            "C. Aglet",
+            "D. Toglet"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quelle est la caractéristique principale des insectes nommés les phasmes ?",
+        answer: "B. Ils ressemblent à des feuilles ou des branches",
+        options:[
+            "A. Ils ne vivent que quelques heures",
+            "B. Ils ressemblent à des feuilles ou des branches",
+            "C. Ils s'attaquent au bois des maisons",
+            "D. Il s'agit d'insecte pollinisateurs"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Que désigne-t-on par l'ancien mot mirliflore ?",
+        answer: "B. Un jeune homme élégant et prétentieux",
+        options:[
+            "A. Un homme qui ne tient pas parole",
+            "B. Un jeune homme élégant et prétentieux",
+            "C. Un menteur invétéré",
+            "D. Un homme généreux"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Combien de départements compte-t-on en France métropolitaine et départements d'outre mer",
+        answer: "C. 101",
+        options:[
+            "A. 97",
+            "B. 99",
+            "C. 101",
+            "D. 103"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quelle marque a été fondée par le frère du créateur de la marque Adidas",
+        answer: "D. Puma",
+        options:[
+            "A. Nivea",
+            "B. BMW",
+            "C. UHU",
+            "D. Puma"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Quel est la capitale du Maroc",
+        answer: "A. Rabat",
+        options:[
+            "A. Rabat",
+            "B. Tanger",
+            "C. Casablanca",
+            "D. Marrakech"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qui est le seul buteur de la finale de l'Euro 2016 opposant la France au Portugal",
+        answer: "B. Eder",
+        options:[
+            "A. Griezmann",
+            "B. Eder",
+            "C. Ronaldo",
+            "D. Mbappé"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Combien de doigts y a-t-il généralement sur les pattes arrière des chiens ?",
+        answer: "B. 4",
+        options:[
+            "A. 3",
+            "B. 4",
+            "C. 5",
+            "D. 6"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Lequel de ces acteurs interprète Vinz dans le film 'La Haine'",
+        answer: "D. Vincent Cassel",
+        options:[
+            "A. Jean Dujardin",
+            "B. Gérard Depardieu",
+            "C. Franck Gastambide",
+            "D. Vincent Cassel"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Qui était le selectionneur de l'équipe de France en 1998",
+        answer: "A. Aimé Jacquet",
+        options:[
+            "A. Aimé Jacquet",
+            "B. Raymond Domenech",
+            "C. Roger Lemerre",
+            "D. Didier Deschamps"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Dans quelle ville italienne la pizza a-t-elle été inventée ?",
+        answer: "C. Naples",
+        options:[
+            "A. Rome",
+            "B. Palerme",
+            "C. Naples",
+            "D. Pise"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Quel est le nouvel âge légal de départ à la retraite adopté par le gouvernement français en 2023 ?",
+        answer: "C. 64 ans",
+        options:[
+            "A. 60 ans",
+            "B. 62 ans",
+            "C. 64 ans",
+            "D. 65 ans"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Quelle formule permet d’associer une vitesse (v) à un distance (d) et un temps (t) ?",
+        answer: "C. d = v x t",
+        options:[
+            "A. v = d x t",
+            "B. t = d x v",
+            "C. d = v x t",
+            "D. v = d² x t"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Quelle est la vitesse d'un chat qui parcourt 12km en un heure et demi ?",
+        answer: "B. 8km/h",
+        options:[
+            "A. 6km/h",
+            "B. 8km/h",
+            "C. 10km/h",
+            "D. 12km/h"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Quel est le plus grand océan du monde ?",
+        answer: "D. L'océan Pacifique",
+        options:[
+            "A. L'océan Atlantique",
+            "B. L'océan Indien",
+            "C. L'océan Arctique",
+            "D. L'océan Pacifique"
+        ]
+    },
+    {
+        numb: 16,
+        question: "Qui a écrit 'Le Petit Prince' ?",
+        answer: "A. Antoine de Saint-Exupéry",
+        options:[
+            "A. Antoine de Saint-Exupéry",
+            "B. Jules Verne",
+            "C. Alexandre Dumas",
+            "D. Victor Hugo"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Quel insecte a littéralement fait une invasion dans certains pays d'Afrique de l'Est en 2020, ayant des conséquences sur l'agriculture ?",
+        answer: "C. Les criquets",
+        options:[
+            "A. Les abeilles",
+            "B. Les capricornes",
+            "C. Les criquets",
+            "D. Les fourmis"
+        ]
+    },
+    {
+        numb: 18,
+        question: "Quelle actrice incarne Ronal dans le film Avatar 2 : la Voie de l'eau ?",
+        answer: "B. Kate Winslet",
+        options:[
+            "A. Cameron Diaz",
+            "B. Kate Winslet",
+            "C. Zoe Saldana",
+            "D. Charlize Theron"
+        ]
+    },
+    {
+        numb: 19,
+        question: "Dans Harry Potter quel est le véritable nom de Voldemort ?",
+        answer: "D. Tom Elvis Jedusor",
+        options:[
+            "A. Paul Billy Jedusor",
+            "B. Ralph Robbie Jedusor",
+            "C. Frank James Jedusor",
+            "D. Tom Elvis Jedusor"
+        ]
+    },
+    {
+        numb: 20,
+        question: "De quel rêve Martin Luther King parlait-il lors sont discours à Washington en 1963 ”I have a dream” ?",
+        answer: "D. La fin de la ségrégation sociale",
+        options:[
+            "A. La fin de l'esclavage",
+            "B. Devenir président des USA",
+            "C. La fin du capitalisme",
+            "D. La fin de la ségrégation sociale"
+        ]
+    }
+];
+
+
